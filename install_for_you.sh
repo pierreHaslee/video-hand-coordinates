c27fbe850ce8c5c2a0a25809773b77675348976f
pierreHaslee <pierre.haslee@protonmail.com>
2022-08-12 13:27:19 +0100

    auto install update

--- install_for_you.sh
+++ install_for_you.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 echo "this script will install all the prerequisites and create a conda virtual environment named 'openmmlab'"
+echo "there are specific steps for cpu-only or for gpu installation, which one do you want?"
+echo "cpu/gpu"
+
+read device
+
 echo "be careful: you need conda installed, and be sure you run this script at the root of this git repo"
 echo "continue ?"
 echo "y/n"
 
 read continue
 
-if [ "$continue" != "y" ]; then
+if [ "$continue" != "y" ] || [ "$device" != "cpu" -a "$device" != "gpu" ]; then
   echo "install cancelled"
 else
-  conda create --name openmmlab python=3.8 -y
+  conda create --name openmmlab python=3.8
   conda activate openmmlab
-  conda install pytorch torchvision -c pytorch
+
+  if [ "$device" == "cpu"]; then
+    conda install pytorch torchvision cpuonly -c pytorch
+  else
+    conda install pytorch torchvision -c pytorch
+  fi
 
   pip install -U openmim
   mim install mmcv-full
@@ -28,7 +38,5 @@ else
   pip install -v -e .
 
   cd ..
-
-  echo "and that should be done now."
 
 fi
